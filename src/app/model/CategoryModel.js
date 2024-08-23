cd0435d0d8a99da701cb1bde8be05d3692713da2
joy11122 <joyhaldar112233@gmail.com>
2024-08-22 22:54:26 -0700

    chat

--- src/app/model/CategoryModel.js
+++
@@ -1,7 +0,0 @@
-import { mongoose } from "mongoose";
-
-const CategoryModel=new mongoose.Schema({
-    name:String,
-    slug:String,
-})
-export const CategorySchema=mongoose.models.category || mongoose.model("category",CategoryModel)
